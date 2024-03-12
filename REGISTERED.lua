661e56fe5513f4bc11320fcd65b102664d9201c8
OptioniaI <95887974+OptioniaI@users.noreply.github.com>
2024-03-11 20:24:48 -0700

    Create REGISTERED.lua

---
+++ REGISTERED.lua
@@ -0,0 +1,9 @@
+return {
+	"Krampus-Fingerprint", "Sw-Fingerprint",
+  "Syn-Fingerprint", "Krnl-Fingerprint",
+  "Sentinel-Fingerprint", "Flux-Fingerprint",
+  "Oxy-Fingerprint", "Evon-Fingerprint",
+  "Kiwi-Fingerprint", "Wrd-Fingerprint",
+  "Valyse-Fingerprint", "Arceus-Fingerprint",
+  "Fingerprint"
+}
