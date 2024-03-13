aacd9c06ba23f3f49af127d0f9176c39917b1549
OptioniaI <95887974+OptioniaI@users.noreply.github.com>
2024-03-12 18:32:20 -0700

    Create WL.lua

---
+++ WL.lua
@@ -0,0 +1,157 @@
+return {
+    -- Krampus PC 'HWID' Section
+
+    "737ac3286f363ca4a372e2ea7aee2db47f833b20ff49291e8bcd1361360f72a8", "2ff11b91e94e12b7b573ba4727832050e8a5aad2c9a3d286e55d04e456a04a8b", "", -- Tactic (765706304656113664, 1st), Nightmare (2nd, 745356151830348016), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Script Ware - MacOS 'HWID' Section
+
+    "457b8db2b48a41e497b6a12b10abbd5a5b8bac94e098bba5153b27c024fd812db5cff203af254755cca1395590b2e6ad", "a1a41d2260886995a81a95194a38635b1d186ee758ddb46bb63e0b12ad7d92ab8fbebca73bfe7595cbe75103fb4d1275", "", -- world (617178707286687766, 1st), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Script Ware - Windows 'HWID' Section
+
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Synapse X 'HWID' Section
+
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Krnl - Latest 'HWID' Section
+
+    "CE906DC0EC8A2D2895E0FA70B1963CC09DC8", "", "", -- ayd (934173550376542279, 1st), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Krnl - Legacy 'HWID' Section
+
+    "D8903A045BDC93B8ABBE0078757E95B088BC", "D8903A045B6400D8B3608A1C686277B088BC", "D8903A045BE21578AF1AB304E69AC3B088BC", -- niko. (726930456125112470, 1st), xzy (765903032751489055, 2nd), O6MN (1079444644670222366, 3rd)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Evon 'HWID' Section
+
+    "82ec927f7fca48632c75bb6a7eb159c8 PAUSED", "7a697fd35309095d9ad370e2fcc5feae", "", -- offset (1065367700798443563, 1st), vmc (947971066301677589, 2nd), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Fluxus 'HWID' Section
+
+    "a70781c25ad1bc0b60e1c6bbc77aa69b", "56803bed9e9f834fff35eeac6218c4c8", "29f6ec3de5899b01629309127973f3a1", -- Juan Pablo (a70781c25ad1bc0b60e1c6bbc77aa69b, 1st), niko. (726930456125112470, 2nd), trix (1091581837689761802, 3rd)
+    "9914fef0ba5000346b03d31cb728013e", "b2bca27f5e3a44ffafb82a26f2d3be2e762cfec8c9421d64a6496edf271f073537c27bc8c574fcc85d45b302728fa957", "adf2dbf973c4941fe4c6ca231f7ae60f", -- JustO6mn (1079444644670222366, 1st), Space (1085343485164519434, 2nd), sbtrhwhsr (934101633435041842, 3rd)
+    "0db78ef1265cad2d040d99552c8ba3a1", "", "", -- Xylex (1086232338247204888, 1st), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Oxygen U 'HWID' Section
+
+    "17949c15f2286a08a4bb6090f9825e8a", "", "", -- ayd (934173550376542279, 1st), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Hydrogen 'HWID' Section
+
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Electron 'HWID' Section
+
+    "b16e9a4053ab4ce0c869da04571dbf7cb4f193dfd29eb1124fdc0ad539cb4a70", "", "", -- Xylex (1086232338247204888, 1st), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+
+    -- Valyse Executor 'HWID' Section
+
+    "c690b66d026ad22a936c16d1868c86a588f5420a24aea2b52cee01c73b499a85", "", "", -- krix (1132228085270458369, 1st), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "",  -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Arceus X 'HWID' Section
+
+    "9e57ffae1da5718300000000a7619e5d51d485e3c2b2a6eb4f6ae6757d77397ee091d278dbdc28499a94196669317ea1", "9dde932e5fd4d8330000000045239d073a1a19b186cbecd59ec0baa8feea035c786acc556b85ede12cd0c762eee501d4", "28033A5A-C46F-427E-9778-42C6AB5E92D2", -- Owner (1, 1st), SOL (2nd, 1122553508185518140), Nightmare (3rd, 745356151830348016)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "",  -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+    -- Other Executor 'HWID' Section
+
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "", "", ""  -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+
+}
