7d9e16d2f03dc3dd1b386d59da143955fbfa9f71
OptioniaI <95887974+OptioniaI@users.noreply.github.com>
2024-03-23 10:22:37 -0700

    Create PERM.lua

---
+++ PERM.lua
@@ -0,0 +1,5 @@
+return {
+  985143698, 5383720593, 2745881210, -- LexiEsMiMami, ChrisEsMiPapi, OptioniaI
+  3534978079, 5458733460, 5677483200, -- urwhitebf, IDominateEveryone, RoExecOnTop
+  3387266948 -- RelatedToLegend
+}
