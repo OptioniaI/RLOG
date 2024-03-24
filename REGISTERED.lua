e6eee10b3a877e427c4c3e61a3af7290c6c9ea0f
OptioniaI <95887974+OptioniaI@users.noreply.github.com>
2024-03-24 15:21:10 -0700

    Delete REGISTERED.lua

--- REGISTERED.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"Krampus-Fingerprint", "Sw-Fingerprint",
-  "Syn-Fingerprint", "Krnl-Fingerprint",
-  "Sentinel-Fingerprint", "Flux-Fingerprint",
-  "Oxy-Fingerprint", "Evon-Fingerprint",
-  "Kiwi-Fingerprint", "Wrd-Fingerprint",
-  "Valyse-Fingerprint", "Arceus-Fingerprint",
-  "Fingerprint"
-}
