aa31acc052746589c098bcc604f9e6b12eaafc1f
OptioniaI <95887974+OptioniaI@users.noreply.github.com>
2024-04-26 13:04:42 -0700

    Update WL.lua

--- WL.lua
+++ WL.lua
@@ -23,7 +23,7 @@ return {
 
       -- Codex 'HWID' Section
 
-    "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
+    "9e57ffae1da5718300000000a7619e5d51d485e3c2b2a6eb4f6ae6757d77397ee091d278dbdc28499a94196669317ea1", "", "", -- Tactic (1st, 765706304656113664), Slot Empty (2), Slot Empty (3)
     "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
     "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
     "", "", "", -- Slot Empty (1), Slot Empty (2), Slot Empty (3)
