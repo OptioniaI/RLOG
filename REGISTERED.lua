1b5594ba77b511d6d25347f92b1f000553161588
OptioniaI <95887974+OptioniaI@users.noreply.github.com>
2025-05-08 23:49:27 -0700

    Update REGISTERED.lua

--- REGISTERED.lua
+++ REGISTERED.lua
@@ -1,8 +1,6 @@
 return {
-    "Codex-Fingerprint", "Arceus-Fingerprint",
-    "Delta-Fingerprint", "Wave-Fingerprint",
-    "Solara-Fingerprint", "Macsploit-Fingerprint",
-    "Swift-Fingerprint", "Xeno-Fingerprint",
-    "Vega-Fingerprint", "Awp-Fingerprint",
-    "Fingerprint"
+    "Awp-Fingerprint", "Wave-Fingerprint", "Swift-Fingerprint",
+    "Macsploit-Fingerprint", "Zenith-Fingerprint", "Delta-Fingerprint",
+    "Solara-Fingerprint", "Codex-Fingerprint", "Vega-Fingerprint",
+    "Xeno-Fingerprint", "Fingerprint"
 }
